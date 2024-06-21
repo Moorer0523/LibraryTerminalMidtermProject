dd573a5213c746901e1ad19c63f92f0dbb15d121
Rob Moore <moorer0523@gmail.com>
2024-06-20 20:17:00 -0400

    removed commented out Enum attributes

--- LibraryTerminalLab/Book.cs
+++ LibraryTerminalLab/Book.cs
@@ -12,17 +12,15 @@ public class Book
 {
     public string Title { get; set; }
     public string Author { get; set; }
-
-    //Need to create enum before this will work correctly:
-    //public BookStatus Status { get; set; }
+    public BookStatus Status { get; set; }
     public DateTime DueDate { get; set; }
     public string Genre { get; set; }
 
-    public Book(string title, string author, /*BookStatus status,*/ string genre) //Need to add status when created
+    public Book(string title, string author, BookStatus status, string genre) //Need to add status when created
     {
         Title = title;
         Author = author;
-        //BookStatus = status;
+        Status = status;
         Genre = genre;
     }
 }
