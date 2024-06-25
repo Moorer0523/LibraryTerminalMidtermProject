1792298a88257d3b2a6ddb597f3c28a43f36a011
Freddie <freddiex06@outlook.com>
2024-06-24 17:23:45 -0700

    Added Catalog

---
+++ LibraryTerminalLab/Catalog.cs
@@ -0,0 +1,23 @@
+﻿namespace LibraryTerminalLab;
+
+public class Catalog;
+
+    List<Book> books = new List<Book>
+        {
+            new Book("1984", "George Orwell", "Dystopian Fiction"),
+            new Book("To Kill a Mockingbird", "Harper Lee", "Southern Gothic"),
+            new Book("The Great Gatsby", "F. Scott Fitzgerald", "Literary Fiction"),
+            new Book("Pride and Prejudice", "Jane Austen", "Romance"),
+            new Book("Brave New World", "Aldous Huxley", "Science Fiction"),
+            new Book("The Catcher in the Rye", "J.D. Salinger", "Coming-of-Age Fiction"),
+            new Book("Catch-22", "Joseph Heller", "Satire"),
+            new Book("Moby-Dick", "Herman Melville", "Adventure Fiction"),
+            new Book("War and Peace", "Leo Tolstoy", "Historical Fiction"),
+            new Book("The Lord of the Rings", "J.R.R. Tolkien", "Fantasy"),
+            new Book("Crime and Punishment", "Fyodor Dostoevsky", "Psychological Fiction"),
+            new Book("Beloved", "Toni Morrison", "Historical Fiction"),
+            new Book("Slaughterhouse-Five", "Kurt Vonnegut", "Science Fiction"),
+            new Book("The Road", "Cormac McCarthy", "Post-Apocalyptic Fiction"),
+            new Book("One Hundred Years of Solitude", "Gabriel Garcia Marquez", "Magical Realism")
+        };
+
